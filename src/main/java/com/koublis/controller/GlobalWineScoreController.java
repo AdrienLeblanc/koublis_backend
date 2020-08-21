479b80871c9ec4c89f7ca1b948e994441752046b
Adrien Leblanc <adrienleblanc53@gmail.com>
2020-08-21 04:42:33 +0200

    Ajout du @RequestMapping(api) dans certains controllers

--- src/main/java/com/koublis/controller/GlobalWineScoreController.java
+++ src/main/java/com/koublis/controller/GlobalWineScoreController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -26,6 +27,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RestController
+@RequestMapping("/api/")
 public class GlobalWineScoreController {
 
     @Autowired
