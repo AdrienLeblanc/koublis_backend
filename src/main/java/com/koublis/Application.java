a10299d0ae20becc68e6fd44abf7631e0c32e35d
Adrien Leblanc <adrienleblanc53@gmaiL.com>
2020-12-01 00:33:26 +0100

    fix mongodb express

--- src/main/java/com/koublis/Application.java
+++ src/main/java/com/koublis/Application.java
@@ -1,14 +1,10 @@
 package com.koublis;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
