7351ab695d431265053d74bdbee49224de64c862
Adrien Leblanc <adrien.leblanc@liksi.fr>
2025-04-22 19:59:16 +0200

    Remove .idea directory from git tracking

--- src/main/java/com/koublis/configuration/KoublisControllerAdvice.java
+++ src/main/java/com/koublis/configuration/KoublisControllerAdvice.java
@@ -4,12 +4,12 @@ import com.koublis.configuration.exceptions.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class KoublisControllerAdvice {
 
     @ExceptionHandler(ResourceNotFoundException.class)
